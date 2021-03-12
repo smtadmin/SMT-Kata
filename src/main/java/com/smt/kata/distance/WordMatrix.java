2c0fcb007e1a698b12a9003974d398e7dd04392b
Etewa <james@siliconmtn.com>
2021-03-12 10:03:09 -0700

    feat(kata): word matrix

--- src/main/java/com/smt/kata/distance/WordMatrix.java
+++ src/main/java/com/smt/kata/distance/WordMatrix.java
@@ -1,8 +1,8 @@
 package com.smt.kata.distance;
 
 // JDK 11.x
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 // Spacelibs 1.x
 import com.siliconmtn.data.text.StringUtil;
@@ -16,16 +16,28 @@ import com.siliconmtn.data.text.StringUtil;
  * up-to-down or down to up.
  * 
  * For example, given the following matrix:
-
- * [['F', 'A', 'C', 'I'],
- * ['O', 'B', 'Q', 'P'],
- * ['A', 'N', 'O', 'B'],
- * ['M', 'A', 'S', 'S']]
+ *
+ *	{
+ *		{ 'F', 'A', 'C', 'I', 'H', 'E', 'L', 'L', 'O', '!' },
+ *		{ 'O', 'B', 'Q', 'P', 'I', 'L', 'R', 'K', 'I', 'M' },
+ *		{ 'A', 'N', 'O', 'B', 'J', 'B', 'D', 'L', 'I', 'E' },
+ *		{ 'M', 'A', 'S', 'S', 'U', 'I', 'F', 'O', 'O', 'A' },
+ *		{ 'K', 'V', 'W', 'Q', 'S', 'B', 'F', 'O', 'V', 'B' },
+ *		{ 'O', 'L', 'A', 'X', 'P', 'E', 'K', 'T', 'A', 'E' },
+ *		{ 'N', 'D', 'V', 'F', 'I', 'E', 'S', 'P', 'M', 'T' },
+ *		{ 'R', 'D', 'P', 'L', 'N', 'R', 'X', 'Q', 'B', 'J' },
+ *		{ 'P', 'U', 'L', 'L', 'G', 'U', 'Q', 'W', 'D', 'I' },
+ *		{ 'Z', 'M', 'B', 'B', 'H', 'I', 'A', 'I', 'Z', 'M' }
+ *	}
  * 
- * and the target word 'FOAM', you should return true, since it's the leftmost 
+ * and the target word 'FOAM', you should return true, since it's in the left most 
  * column. Similarly, given the target word 'MASS', you should return true, 
- * since it's the last row.
+ * since it's in the fourth row.
  * 
+ * Bonus activities:
+ * Find words that are diagonally oriented in both directions as well (the word BOSSES goes diagonal 
+ * from position 1,2)
+
  * <b>Copyright:</b> Copyright (c) 2021
  * <b>Company:</b> Silicon Mountain Technologies
  * 
@@ -37,12 +49,18 @@ import com.siliconmtn.data.text.StringUtil;
 public class WordMatrix {
 	
 	// Members
-	private List<String> words = new ArrayList<>();
+	private Set<String> words = new HashSet<>();
 	private char[][] matrix = new char[][] {
-		{ 'F', 'A', 'C', 'I' },
-		{ 'O', 'B', 'Q', 'P' },
-		{ 'A', 'N', 'O', 'B' },
-		{ 'M', 'A', 'S', 'S' }
+		{ 'F', 'A', 'C', 'I', 'H', 'E', 'L', 'L', 'O', '!' },
+		{ 'O', 'B', 'Q', 'P', 'I', 'L', 'R', 'K', 'I', 'M' },
+		{ 'A', 'N', 'O', 'B', 'J', 'B', 'D', 'L', 'I', 'E' },
+		{ 'M', 'A', 'S', 'S', 'U', 'I', 'F', 'O', 'O', 'A' },
+		{ 'K', 'V', 'W', 'Q', 'S', 'B', 'F', 'O', 'V', 'B' },
+		{ 'O', 'L', 'A', 'X', 'P', 'E', 'K', 'T', 'A', 'E' },
+		{ 'N', 'D', 'V', 'F', 'I', 'E', 'S', 'P', 'M', 'T' },
+		{ 'R', 'D', 'P', 'L', 'N', 'R', 'X', 'Q', 'B', 'J' },
+		{ 'P', 'U', 'L', 'L', 'G', 'U', 'Q', 'W', 'D', 'I' },
+		{ 'Z', 'M', 'B', 'B', 'H', 'I', 'A', 'I', 'Z', 'M' }
 	};
 
 	/**
@@ -52,6 +70,8 @@ public class WordMatrix {
 		super();
 		assignVerticalWords();
 		assignHorizontalWords();
+		assignDiagonalHorizontalWords();
+		assignDiagonalVerticalWords();
 	}
 
 	/**
@@ -97,4 +117,43 @@ public class WordMatrix {
 		}
 	}
 	
+	/**
+	 * Moves the pointer horizontally along the matrix and grabs all of the 
+	 * horizontally aligned words
+	 */
+	private void assignDiagonalHorizontalWords() {
+		StringBuilder word = null;
+		
+		for (int i=0; i < matrix.length; i++) {
+			int x = i;
+			word = new StringBuilder();
+			for (int j = 0; j < matrix.length; j++) {
+				int y = j;
+				if (x + 1 > matrix.length) break;
+				word.append(matrix[y++][x++]);
+			}
+			
+			words.add(word.toString());
+		}
+	}
+	
+	/**
+	 * Moves the pointer horizontally along the matrix and grabs all of the 
+	 * horizontally aligned words
+	 */
+	private void assignDiagonalVerticalWords() {
+		StringBuilder word = null;
+		
+		for (int i=0; i < matrix.length; i++) {
+			int x = i;
+			word = new StringBuilder();
+			for (int j = 0; j < matrix.length; j++) {
+				int y = j;
+				if (x + 1 > matrix.length) break;
+				word.append(matrix[x++][y++]);
+			}
+			
+			words.add(word.toString());
+		}
+	}
 }
