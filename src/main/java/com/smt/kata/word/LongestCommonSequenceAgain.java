e1ad6d0e8aab58597608d1a34c0ccd0c72ff6a5b
smt-etewa <james@siliconmtn.com>
2021-08-12 22:59:01 -0600

    Added the new Longest Common Sequence Kata

--- src/main/java/com/smt/kata/word/LongestCommonSequenceAgain.java
+++ src/main/java/com/smt/kata/word/LongestCommonSequenceAgain.java
@@ -1,12 +1,27 @@
 package com.smt.kata.word;
 
+// JDK 11.x
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /****************************************************************************
  * <b>Title</b>: LongestCommonSequenceAgain.java
  * <b>Project</b>: SMT-Kata
- * <b>Description: </b> CHANGE ME!!
+ * <b>Description: </b> Longest Common Sequence Again Kata
+ *  
+ * Write a program that computes the length of the longest common subsequence of 
+ * a variable number of given strings. For example, given "epidemiologist", "refrigeration", 
+ * and "believingmore", it should return "eieio" since that is the longest common subsequence.
+ * 
+ * The sequences are case sensitive.  The sequences do not need to be sequntial,
+ * though they must be in order.
+ * 
+ * Another Example
+ * Given: "bat", "hat", "cat"
+ * Return: "at"
+ * 
  * <b>Copyright:</b> Copyright (c) 2021
  * <b>Company:</b> Silicon Mountain Technologies
  * 
@@ -16,49 +31,97 @@ import java.util.List;
  * @updates:
  ****************************************************************************/
 public class LongestCommonSequenceAgain {
-
-
+	private StringBuilder output;
+	
 	/**
-	 * Loads and returns the possible permutations of a word
-	 * @param word
-	 * @return
+	 * Calculates the longest matching sequence of the words
+	 * @param words Words to evaluate
+	 * @return Longest sequence in all words
 	 */
-    public String getSequence(String[] words) {
-        if(words == null) return "";
-        String sequence = "";
-        
-    	List<List<String>> items = new ArrayList<>();
-    	
-        //Call recursive function
-        for (String word : words) {
-        	List<String> item = new ArrayList<>();
-        	permutation("", word, item);
-        	items.add(item);
-        	
-        	System.out.println(item);
-        }
-        
-        return sequence;
-    }
-    
-    /**
-     * Recursive method to get the permutations
-     * @param permutation
-     * @param word
-     */
-    private void permutation(String permutation, String word, List<String> item) {
-        //Check the base case
-        if (word.isEmpty()) {
-            item.add(permutation + word);
-
-        } else {
-            //iterate through all of the characters
-            for (int i = 0; i < word.length(); i++) {
-                //Call recursive function with sub strings
-                permutation(permutation + word.charAt(i), word.substring(0, i) + word.substring(i + 1, word.length()), item);
-            }
-        }
-
-    }
-
+	public String getSequence(String[] wordsArray) {
+		// Get clean data
+		List<String> words = cleanInput(wordsArray);
+		if (words.size() < 2) return "";
+		
+		// Holder for all possible sequences of each word
+		List<Set<String>> allSequences = new ArrayList<>();
+		
+		// Evaluate each word and get all possible combinations
+		for(String word : words) {
+			Set<String> data = new HashSet<>(2048);
+			output = new StringBuilder();
+			combine(0, word, data);
+			
+			// Add the set of combinations to the list
+			allSequences.add(data);
+		}
+		
+		// Get a corss section of matches against the word
+		Set<String> matches = crossSection(allSequences);
+		
+		// Return the longest match
+		return getLongestSequence(matches);
+	}
+	
+	/**
+	 * Converts the array to a collection and checks each array item for a non-empty word
+	 * @param wordsArray Array of strings ot clean
+	 * @return List of words.  Empty of null or if each item in array is empty
+	 */
+	private List<String> cleanInput(String[] wordsArray) {
+		List<String> words = new ArrayList<>();
+		if (wordsArray == null) return words;
+		
+		for (String word : wordsArray) {
+			if (word == null || word.length() == 0) continue;
+			words.add(word);
+		}
+		
+		return words;
+	}
+	
+	/**
+	 * Based upon the provided matches, find the longest sequence
+	 * @param matches Matched sequences across all of the words
+	 * @return Longest matching sequence
+	 */
+	private String getLongestSequence(Set<String> matches) {
+		String longest = "";
+		
+		for (String match : matches) {
+			if (match.length() > longest.length()) longest = match;
+		}
+		
+		return longest;
+	}
+	
+	/**
+	 * Find all of the matching sequences against the mates for each word
+	 * @param allSequences A collection of matching sequences fo reach word
+	 * @return matching sequences
+	 */
+	private Set<String> crossSection(List<Set<String>> allSequences) {
+		Set<String> first = allSequences.get(0);
+		
+		for (int i = 1; i < allSequences.size(); i++) {
+			first.retainAll(allSequences.get(i));
+		}
+		
+		return first;
+	}
+	
+	/**
+	 * Gets all possible combinations of the provided word
+	 * @param start Start index
+	 * @param word Word being evaluated
+	 * @param data Collection to store each combination
+	 */
+	private void combine(int start, String word, Set<String> data) {
+		for (int i = start; i < word.length(); ++i) {
+			output.append(word.charAt(i));
+			data.add(output.toString());
+			if (i < word.length()) combine(i + 1, word, data);
+			output.setLength(output.length() - 1);
+		}
+	}
 }
--- src/test/java/com/smt/kata/word/LongestCommonSequenceAgainTest.java
+++ src/test/java/com/smt/kata/word/LongestCommonSequenceAgainTest.java
@@ -1,13 +1,13 @@
 package com.smt.kata.word;
 
+// Junit 5
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
 /****************************************************************************
  * <b>Title</b>: LongestCommonSequenceAgainTest.java
  * <b>Project</b>: SMT-Kata
- * <b>Description: </b> CHANGE ME!!
+ * <b>Description: </b> UNit tests for the Longest Common Sequence Again Kata
  * <b>Copyright:</b> Copyright (c) 2021
  * <b>Company:</b> Silicon Mountain Technologies
  * 
@@ -19,13 +19,69 @@ import org.junit.jupiter.api.Test;
 class LongestCommonSequenceAgainTest {
 	
 	private LongestCommonSequenceAgain lcsa = new LongestCommonSequenceAgain();
+	
+	/**
+	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
+	 */
+	@Test
+	void testGetSequenceArrayNull() throws Exception {
+		assertEquals("", lcsa.getSequence(null));
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
+	 */
+	@Test
+	void testGetSequenceArrayAllNull() throws Exception {
+		assertEquals("", lcsa.getSequence(new String[] {null, null }));
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
+	 */
+	@Test
+	void testGetSequenceArrayAllEmpty() throws Exception {
+		assertEquals("", lcsa.getSequence(new String[] {"", "" }));
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
+	 */
+	@Test
+	void testGetSequenceArrayOneValid() throws Exception {
+		assertEquals("", lcsa.getSequence(new String[] {"Test" }));
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
+	 */
+	@Test
+	void testGetSequenceArrayOneValidWithNulls() throws Exception {
+		assertEquals("", lcsa.getSequence(new String[] {"Test", null }));
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
+	 */
+	@Test
+	void testGetSequenceSimple() throws Exception {
+		assertEquals("at", lcsa.getSequence(new String[]{ "cat", "after", "another" }));
+	}
 
 	/**
 	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
 	 */
 	@Test
 	void testGetSequence() throws Exception {
-		assertEquals("", lcsa.getSequence(new String[]{ "cat" }));
+		assertEquals("eieio", lcsa.getSequence(new String[]{ "epidemiologist", "refrigeration", "believingmore" }));
+	}
+	
+	/**
+	 * Test method for {@link com.smt.kata.word.LongestCommonSequenceAgain#getSequence(java.lang.String[])}.
+	 */
+	@Test
+	void testGetSequenceMixedCase() throws Exception {
+		assertEquals("eig", lcsa.getSequence(new String[]{ "EpidemiOlogist", "refrigErAtion", "bElievingmore" }));
 	}
 
 }
