acbb01dc0310648848cfa05b948dde04273135bc
smt-etewa <james@siliconmtn.com>
2021-08-17 10:41:39 -0600

    Updated comments

--- src/main/java/com/smt/kata/number/SpreadsheetNumbering.java
+++ src/main/java/com/smt/kata/number/SpreadsheetNumbering.java
@@ -3,7 +3,14 @@ package com.smt.kata.number;
 /****************************************************************************
  * <b>Title</b>: SpreadsheetNumbering.java
  * <b>Project</b>: SMT-Kata
- * <b>Description: </b> CHANGE ME!!
+ * <b>Description: </b> Spreadsheet Numbering
+ * 
+ * Spreadsheets often use this alphabetical encoding for its columns: "A", "B", 
+ * "C", ..., "AA", "AB", ..., "ZZ", "AAA", "AAB", ....
+ * 
+ * Given a column number, return its alphabetical column id. For example, given 1, 
+ * return "A". Given 27, return "AA".
+ * 
  * <b>Copyright:</b> Copyright (c) 2021
  * <b>Company:</b> Silicon Mountain Technologies
  * 
