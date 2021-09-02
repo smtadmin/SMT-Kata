b61be9029e61a90a0bdac1214efb5ec762404fc3
smt-etewa <james@siliconmtn.com>
2021-09-02 17:19:35 -0600

    Updated the RotatedArray Kata

--- src/main/java/com/smt/kata/number/RotatedArray.java
+++ src/main/java/com/smt/kata/number/RotatedArray.java
@@ -4,7 +4,28 @@ package com.smt.kata.number;
 /****************************************************************************
  * <b>Title</b>: RotatedArray.java
  * <b>Project</b>: SMT-Kata
- * <b>Description: </b> CHANGE ME!!
+ * <b>Description: </b> Rotated Array Kata
+ * 
+ * Search for the index of an element inside an array.  
+ * However, the array will be pivoted at the provided pivot value such that the
+ * array will be reorder from that pivot. For example:
+ * 
+ * Example 1
+ * method: array, target value, pivot
+ * input: [6,5,4,3,2,1,0], 3, 2
+ * New array order: [3,2,1,0,6,5,4]
+ * output 0 (target of 3 is in the 0 array location)
+ * 
+ * Example 1, Pivot is auto calculated by getting the number of elements, halved and
+ * then subtract one for the 0 location.  So the pivot would be ((8/2) -1) = 3
+ * method: array, target value
+ * input: [6,5,4,3,2,1,0,8], 3
+ * New array order: [2,1,0,8,6,5,4,3]
+ * output 7 (target of 3 is in the 7 array location)
+ * 
+ * Note:
+ * if the target number is not in the array, return -1
+ * 
  * <b>Copyright:</b> Copyright (c) 2021
  * <b>Company:</b> Silicon Mountain Technologies
  * 
