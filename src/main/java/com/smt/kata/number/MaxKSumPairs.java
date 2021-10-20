9aebc10c0b899fc20e562f7403956f645fe446c9
smt-etewa <james@siliconmtn.com>
2021-10-20 08:11:27 -0600

    Added the index matcher kata and updated a few unit tests for other katas

--- src/main/java/com/smt/kata/number/MaxKSumPairs.java
+++ src/main/java/com/smt/kata/number/MaxKSumPairs.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
  * 
  * You are given an integer array nums and an integer k.
  * 
- * In one operation, you can pick two numbers from the array whose sum equals k 
+ * In one operation, you can any combination numbers from the array whose sum equals k 
  * and remove them from the array.
  * 
  * Return the maximum number of operations you can perform on the array.
